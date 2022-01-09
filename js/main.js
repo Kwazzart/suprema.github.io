ea63c1fcc80f21f5159ef290c2d7952b802c87df
kwazzart <vladimirkwaz@yandex.ru>
2022-01-09 23:16:43 +0300

    НАчал

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const waitTime = 1000
+const block1 = document.querySelector(".block-1")
+
+
+changeStyle = (element, color="red", text) => {
+    element.style.backgroundColor = color
+}
+changeContent = (e, content) => {
+    e.textContent = content
+}
+
+console.log(block1.innerHTML)
+
+
+
+
